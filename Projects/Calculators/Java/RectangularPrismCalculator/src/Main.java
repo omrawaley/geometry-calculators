5f263370a4daad232f792ed6f051e43bf91756fd
Om Rawaley <133281331+omrawaley@users.noreply.github.com>
2023-05-13 10:05:39 -0400

    Add files via upload

---
+++ Projects/Calculators/Java/RectangularPrismCalculator/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("——— Rectangular Prism / Cube Calculator — Made by Om Rawaley ———");
+
+        System.out.print("w: ");
+
+        float w = s.nextFloat();
+
+        System.out.print("h: ");
+
+        float h = s.nextFloat();
+
+        System.out.print("l: ");
+
+        float l = s.nextFloat();
+
+        float faceArea = w * h;
+
+        float volume = faceArea * l;
+
+        float surfaceArea = 2 * (w * l + h * l + w * h);
+
+        System.out.println("\nVolume: " + volume);
+        System.out.println("Surface Area: " + surfaceArea);
+    }
+}
